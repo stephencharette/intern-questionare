681a5cb3c632094c12f8c5d9f994a0efed2b7c63
Stephen Charette <scharette@solutionstreet.com>
2021-06-01 13:31:50 -0400

    fixed questions

--- questionare.rb
+++ questionare.rb
@@ -2,26 +2,26 @@ require 'json'
 
 puts "INTERN QUESTIONARE"
 print "Please enter your name for documentation purposes, the following information will not be sold
-or distributed."
+or distributed. "
 
 name = gets.chomp
 print "Question #1: If you had a job before this internship, what was it and did you enjoy your time there? "
 one = gets.chomp
-print "Question #2: What is a hobby that you enjoy? "
+print "Question #2: What is a *unique if possible* hobby that you enjoy? "
 two = gets.chomp
 print "Question #3: Do you follow any sports teams, or if not, have you played any sports in the past? "
 three = gets.chomp
 print "Question #4: What college are you attending and when is your expected graduation year? "
 four = gets.chomp
-print "Question #5: Do you know any hidden gem dinning places that you really enjoy? "
+print "Question #5: Do you know any hidden gem dining/food places that you really enjoy? "
 five = gets.chomp
 print "Question #6: Have you ever been hiking before and if so where/which trail did you go hiking on? "
 six = gets.chomp
-print "Question #7:  "
+print "Question #7: Roughly how long is your commute here? "
 seven = gets.chomp 
-print "Question #8: If you were able to work completely remote, where would you live? "
+print "Question #8: If you were able to work completely remote, where would you live and why? "
 eight = gets.chomp
-print "Question #9: "
+print "Question #9: What streaming service would you recommend? *excluding Hulu* "
 nine = gets.chomp
 print "Question #10: What is your opinion on pickles? "
 ten = gets.chomp
